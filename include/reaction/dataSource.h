d59ca037da4aa6ee083b49643504149597222d0f
kikyousam <2799383344@qq.com>
2025-07-08 20:47:48 +0800

    first step finsh three classes

---
+++ include/reaction/dataSource.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "reaction/expression.h"
+
+namespace reaction
+{
+    template<typename T, typename... Args>
+    class DataSource : public Expression<T, Args...>  //用来和用户交互, 采用继承的方式表示is a的关系
+    {
+    public:
+        DataSource(const DataSource& d) {}
+        using Expression<T, Args...>::Expression;
+        auto get() const {
+            return this->getValue();
+        }
+    };
+
+    template <typename T>
+    auto var(T &&t) {
+        return DataSource<std::decay_t<T>>(std::forward<T>(t)); //参数是万能引用，根据左值还是右值引用执行相应的构造函数
+    }
+
+    template <typename Func, typename... Args>
+    auto calc(Func &&fun, Args &&...args) {
+        return DataSource<std::decay_t<Func>, std::decay_t<Args>...>(std::forward<Func>(fun), std::forward<Args>(args)...); //参数是万能引用，根据左值还是右值引用执行相应的构造函数, 类的类型参数需要去引用
+    }
+}
---
+++ include/reaction/resource.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+#include <exception>
+
+namespace reaction
+{
+    template<typename Type>
+    class Resource
+    {
+    public:
+        // Resource(Type &&t) :m_ptr<std::make_unique<Type>(std::forward<Type>(t)) {}// 引用折叠不能作用于长生命周期的对象，可以用于函数
+        Resource() : m_ptr(nullptr) {}
+        template<typename T>
+        Resource(T &&t) :m_ptr(std::make_unique<Type>(std::forward<T>(t))) {}
+
+        Resource(const Resource &) = delete;
+        Resource& operator=(const Resource&) = delete;
+        
+        Type &getValue() const { 
+            if(!m_ptr) {
+                throw std::runtime_error("Resource is not initialized");
+            }
+            return *m_ptr; 
+        }
+
+        template<typename T>
+        void updateValue(T &&t) {
+            if(!m_ptr) {
+                m_ptr = std::make_unique<Type>(std::forward<T>(t));
+            }
+            *m_ptr = std::forward<T>(t);
+        }
+    private:
+        std::unique_ptr<Type> m_ptr; 
+    };
+}
